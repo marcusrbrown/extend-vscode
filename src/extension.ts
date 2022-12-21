06f5ab8958bc294cfede2a572eca4a2e7bdd13b8
Marcus R. Brown <contact@marcusrbrown.com>
2022-12-21 00:41:38 -0700

    feat: merge the `feat/ext-from-generator-code-1.7.0` branch

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,9 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext): void {
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "vscode-bash" is now active!');
@@ -14,10 +14,12 @@ export function activate(context: vscode.ExtensionContext): void {
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand('vscode-bash.helloWorld', () => {
     // The code you place here will be executed every time your command is executed
-
     // Display a message box to the user
     vscode.window.showInformationMessage('Hello World from vscode-bash!');
   });
 
   context.subscriptions.push(disposable);
 }
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
