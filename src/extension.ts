10991e86b37857a8c0feee76bb649f975e83f619
Marcus R. Brown <contact@marcusrbrown.com>
2024-06-13 02:26:44 -0700

    chore: rename project to `extend-vscode`

--- src/extension.ts
+++ src/extension.ts
@@ -7,18 +7,18 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vscode-bash" is now active!');
+  console.log('Congratulations, your extension "extend-vscode" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
-    'vscode-bash.helloWorld',
+    'extend-vscode.helloWorld',
     () => {
       // The code you place here will be executed every time your command is executed
 
       // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from vscode-bash!');
+      vscode.window.showInformationMessage('Hello World from extend-vscode!');
     },
   );
 
